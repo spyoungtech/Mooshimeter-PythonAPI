1b96f5952b68aadc0895ae4bd492b8aaa9a30fbe
Spencer Young <spencer.young@spyoung.com>
2018-12-26 16:33:25 -0800

    add metadata

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     url='https://github.com/spyoungtech/Mooshimeter-PythonAPI',
     license='GPLv3',
     author='James Whong',
+    author_email='hello@moosh.im',
     maintainer='Spencer Young',
     maintainer_email='spencer.young@spyoung.com',
     description='UNOFFICIAL! This package is a fork from the original mooshimeter python API',
